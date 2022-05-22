b285976449f28c1838b3a02992582290af392ab0
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2022-05-22 23:36:14 +0530

    k

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ with body:
         st.write(printff,"check",startd,aa)
 
     
-    dir = os.listdir(path = os.path.join(current_path,"Chatistics"))
+    dir = os.listdir(path = os.path.join(current_path,"Chatistics","raw_data"))
     st.write(dir,current_path)
 
     a = st.text_input( ' type here to see below ')
