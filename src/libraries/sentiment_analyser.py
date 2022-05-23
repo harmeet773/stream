fd1b94072a3ec232a7961d632d9f7d99a5de5834
Gourav Atre <gouravkumar1815@gmail.com>
2022-05-23 21:16:42 +0530

    env.yml

--- src/libraries/sentiment_analyser.py
+++ src/libraries/sentiment_analyser.py
@@ -1,18 +1,18 @@
+import pickle
 
 class GroupSentimentAnalyser():
 
     @classmethod
-    def find_sentiment(cls,df):
-        # TrainedModel.predict(cls.config['input_data'])
+    def find_sentiment(cls,data):
+        model_filename = "Sentiment classifier.sav"
 
-        # #iterating on rows in dataframe
-        # for index, row in df.iterrows():
+        my_knn_model = pickle.load(open(model_filename, 'rb')) # the model will be read into the object my_knn_model 
+                                                            # and you can use the same model to predict the new data.
 
-        #     #adding a new columns with value in the row
-        #     # df.loc[index, 'Timestamp'] = datetime.datetime.now().strftime('%c')
-        #     print(row["text"])
-        #     # print(datetime.fromtimestamp(row["timestamp"]).strftime("%A, %B %d, %Y %I:%M:%S"))
-        #     break
+        result = my_knn_model.predict(data) # X_test is where the new data that is to be predicted is inserted, 
+                                            # here X_test is a part of dataset 
+ 
 
-
+        if result:
+            return result
         return "Not detected"
